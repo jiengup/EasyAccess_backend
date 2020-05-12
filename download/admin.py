c2e5860084c8bf8605f1cef044b732a3b641dea0
jiengupxing <jiengup@gamil.com>
2020-05-12 20:34:31 +0800

    data import 20200512

--- download/admin.py
+++ download/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Course, Resource
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Resource)
--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import NewCategory, News, Banner, Comment
 # Register your models here.
+admin.site.register(NewCategory)
+admin.site.register(News)
+admin.site.register(Banner)
+admin.site.register(Comment)
